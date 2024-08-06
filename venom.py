ffe1dbc933400448a138af470bff21e477fdba3f
Tanishmali <abhijeetkamat789@gmail.com>
2024-08-06 22:59:34 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7497004701:AAFezQ45yWpNgPQ-m8OGmw8MXdR1aTKp-fo'
+ADMIN_ID = '6788750651'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
